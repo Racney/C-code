3265fbb506cc3c3294393daa1120a9110abf31d1
Racney <rcyou@arizona.edu>
2024-10-03 14:03:44 -0700

    Add files via upload

---
+++ firstcode/main.c
@@ -0,0 +1,63 @@
+//#include <stdio.h> //read our keyboard and output our code into screen
+//#include <stdlib.h>
+
+//int main() //the code started here
+//{
+//    printf("Hello this world!\n");
+//    return 0;//the system will assume the code is over when it turn back to zero
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    printf("Hello \nWorld!\n");//string
+//    return 0;
+//}
+
+//#include <stdio.h> //challenge one:clear name here
+//
+//int main()
+//{
+//    printf("First name:Randy \nLast name:You\n");
+//    return 0;
+//}
+
+//#include <stdio.h> //method one : use three different printf
+//
+//int main()
+//{
+//    printf("Full name : Randy Chen YOu\n");
+//    printf("Age : 24\n");
+//    printf("Gender : male\n");
+//    return 0;
+//}
+
+//#include <stdio.h>  //method two : use one printf
+//
+//int main()
+//{
+//    printf("Full name:Randy Chen You \nAge: 24 \nGender:Male\n");
+//    return 0;
+//
+//}
+
+//#include <stdio.h>  //Challenge 3: an Isosceles Triangle, three printf line
+//
+//int main()
+//{
+//    printf("  *\n");
+//    printf(" ***\n");
+//    printf("*****\n");
+//    return 0;
+//}
+
+//#include <stdio.h> //challenge 3 :an Isosceles Triangle , one printf
+//
+//int main()
+//{
+//    printf("  *\n ***\n*****\n");
+//    return 0;
+//}
+
+
